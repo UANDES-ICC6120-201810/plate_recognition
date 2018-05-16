25b21df861e9044c2dc098cb0db46f91c20cbb3e
José Tomás González <jtgonzalez@miuandes.cl>
2018-05-16 14:25:08 -0400

    Updated "start.sh" script.

--- start.sh
+++ start.sh
@@ -2,16 +2,16 @@
 
 TOTAL_STEPS="4"
 
-echo "(1 of $TOTAL_STEPS) Compiling SVM training" \
+echo "Step 1/$TOTAL_STEPS : Compiling SVM training" \
 && g++ TrainSVM.cpp core/svm_char_detector.cpp $(pkg-config --libs opencv) -o TrainSVM \
 && echo "SVM compiled correctly!"\
 && echo \
-&& echo "(2 of $TOTAL_STEPS) Training SVM" \
+&& echo "Step 2/$TOTAL_STEPS : Training SVM" \
 && ./TrainSVM \
 && echo \
-&& echo "(3 of $TOTAL_STEPS) Compiling ALPR" \
+&& echo "Step 3/$TOTAL_STEPS : Compiling ALPR" \
 && g++ MainALPR.cpp core/plate_segmentation.cpp core/char_segmentation.cpp core/ocr_detector.cpp core/svm_char_detector.cpp core/constants.hpp core/debugger.cpp $(pkg-config --libs opencv) -o MainALPR \
 && echo "ALPR compiled correctly!" \
 && echo \
-&& echo "(4 of $TOTAL_STEPS) Starting ALPR" \
+&& echo "Step 4/$TOTAL_STEPS : Starting ALPR" \
 && ./MainALPR
