200f48d15c5297da1c12516b7b894f2f393e9cd2
José Tomás González <jtgonzalez@miuandes.cl>
2018-05-21 22:04:30 -0400

    Start: Added condition if git pull fails.

--- start.sh
+++ start.sh
@@ -5,4 +5,10 @@ echo "Pulling from git repo"
 echo
 git pull
 
-./scripts/start.sh
+if [ $? -eq 0 ]
+then
+    ./scripts/start.sh
+else
+    echo
+    echo "Git pull failed, check with 'git status'"
+fi
