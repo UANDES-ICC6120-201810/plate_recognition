5d03f09f61be7fbf679cd3cc9a085fddb5695749
José Tomás González <jtgonzalez@miuandes.cl>
2018-05-21 22:22:21 -0400

    Start: The camera should run the last version even if the "git pull" fails

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ git pull > /dev/null 2>&1
 if [ $? -eq 0 ]
 then
     echo "Git pull succeeded"
-    ./scripts/start.sh
 else
     echo "Git pull failed, check with 'git status'"
 fi
+./scripts/start.sh
