48e562053f1a9e6a23328ddd74032a7273bd318f
José Tomás González <jtgonzalez@miuandes.cl>
2018-06-11 13:30:00 -0400

    Bin: Created empty binary directory for compiled programs
    Scripts: Update exit status for compiler scripts

--- scripts/compile_alpr.sh
+++ scripts/compile_alpr.sh
@@ -10,5 +10,7 @@ then
     echo "ALPR compiled correctly!"
 else
     echo "ALPR compile failed"
+    popd > /dev/null 2>&1
+    exit 1
 fi
 popd > /dev/null 2>&1
--- scripts/train_svm.sh
+++ scripts/train_svm.sh
@@ -30,6 +30,7 @@ else
         ./TrainSVM
     else
         echo "SVM compile failed"
+        popd > /dev/null 2>&1
         exit 1
     fi
 fi
