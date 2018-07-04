74c702d39fbf9e10a8fad7bb30f5d6d403dac9e5
José Tomás González <jtgonzalez@miuandes.cl>
2018-07-04 16:27:23 -0400

    Mysql Connector: Added mutex for critical section.

--- core/mysql_connector.cpp
+++ core/mysql_connector.cpp
@@ -8,12 +8,15 @@
 #include <string>
 #include <thread>
 #include <unistd.h>
+#include <mutex>
 
 #include "mysql_connection.h"
 #include <cppconn/driver.h>
 #include <cppconn/resultset.h>
 #include <cppconn/statement.h>
 #include <cppconn/exception.h>
+
+std::mutex post_mutex;
 
 MysqlConnector::MysqlConnector( std::string host, std::string user, std::string password, std::string database ) {
     while ( true ) {
@@ -42,19 +45,21 @@ void MysqlConnector::post( std::string plate ) {
     int inserted_rows = 0;
 
     int post_tries = 1;
-    int max_post_tries = 20;
+    int max_post_tries = 2;
 
     while ( inserted_rows == 0 && post_tries <= max_post_tries ) {
         std::cout << "[Info] Try " << post_tries << " of " << max_post_tries << ": Inserting '" << plate << "' into plate_readings" << std::endl;
         
+        post_mutex.lock();
         try {
             inserted_rows = statement -> executeUpdate( "INSERT INTO plate_readings(plate) VALUES ('" + plate + "')" );
             std::cout << "[Info] Post of plate'" << plate << "' was successful!" << std::endl;
-
+            
         } catch ( sql::SQLException &e ) {
             std::cout << "[Error] Got SQLException while inserting into DB! Retrying..." << std::endl;
             usleep( 1000000 );
         }
+        post_mutex.unlock();
 
         post_tries++;
     }
